8fb5cf9db74c2be3ff82ef0cf3e3881bd9eee2ae
Adrian Grillo <64506487+AdrianGrillo@users.noreply.github.com>
2020-09-19 15:05:03 -0400

    Add files via upload

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,49 +1,78 @@
 import React from 'react'
+import Axios from 'axios'
 
-function fetchReducer(state, action) {
-    if(action.type === 'fetch') {
-        return {
-            ...state,
-            loading: true
-        }
-    } else if(action.type === 'success') {
-        return {
-            ...state,
-            loading: false,
-            data: action.data
-        }
-    } else if(action.type === 'error') {
-        return {
-            ...state,
-            loading: false,
-            error: "Error fetching gifs"
-        }
-    } else {
-        throw new Error('Action not supported, try again.')
-    }
-}
+// function fetchReducer(state, action) {
+//     if(action.type === 'fetch') {
+//         return {
+//             ...state,
+//             loading: true
+//         }
+//     } else if(action.type === 'success') {
+//         return {
+//             ...state,
+//             loading: false,
+//             data: action.data
+//         }
+//     } else if(action.type === 'error') {
+//         return {
+//             ...state,
+//             loading: false,
+//             error: "Error fetching gifs"
+//         }
+//     } else {
+//         throw new Error('Action not supported, try again.')
+//     }
+// }
+
+// export default function useFetch(url) {
+//     const [state, dispatch] = React.useReducer(
+//         fetchReducer,
+//         { data: null, error: null, loading: true }
+//     )
+
+//     React.useEffect(() => {
+//         dispatch({ type: 'fetch' })
+
+//         fetch(url)
+//             .then((res) => res.json())
+//             .then((data) => dispatch({ type: 'success', data }))
+//             .catch((e) => {
+//                 console.warn(e)
+//                 dispatch({ type: 'error' })
+//             })
+//     }, [url])
+
+//     return {
+//         loading: state.loading, 
+//         data: state.data, 
+//         error: state.error
+//     }
+// }
 
 export default function useFetch(url) {
-    const [state, dispatch] = React.useReducer(
-        fetchReducer,
-        { data: null, error: null, loading: true }
-    )
+    const [error, setError] = React.useState(false)
+    const [loading, setLoading] = React.useState(true)
+    const [data, setData] = React.useState([])
 
     React.useEffect(() => {
-        dispatch({ type: 'fetch' })
-
-        fetch(url)
-            .then((res) => res.json())
-            .then((data) => dispatch({ type: 'success', data }))
-            .catch((e) => {
-                console.warn(e)
-                dispatch({ type: 'error' })
-            })
+        setLoading(true)
+        
+        Axios({
+            method: 'GET',
+            url: url,
+        }).then(res => {
+            setData(res.data)
+            setLoading(false)
+        })
+        .catch(e => {
+            console.warn(e)
+            e.message ? setError(e.message) : setError(false)
+        })
     }, [url])
 
     return {
-        loading: state.loading, 
-        data: state.data, 
-        error: state.error
+        error,
+        loading, 
+        data
     }
 }
--- src/hooks/useInfiniteScroll.js
+++ src/hooks/useInfiniteScroll.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import Axios from 'axios'
 
-function infiniteReducer(state, action) {
+function scrollReducer(state, action) {
     if(action.type === 'fetch') {
         return {
             ...state,
@@ -10,43 +11,138 @@ function infiniteReducer(state, action) {
         return {
             ...state,
             loading: false,
-            data: action.data
+            // data: prevData => {
+            //     return [...prevData, action.res.data]
+            // }
+            data: action.res.data
         }
     } else if(action.type === 'error') {
         return {
             ...state,
             loading: false,
-            error: "Error fetching gifs"
+            error: 'Error fetching data'
         }
     } else {
-        throw new Error('Action not supported, try again.')
+        throw new Error('Action not supported')
     }
 }
 
-export default function useInfiniteScroll(url) {
+const initialState = {
+    loading: true,
+    error: null,
+    data: []
+}
+
+export default function useInfiniteScroll(url, offset) {
     const [state, dispatch] = React.useReducer(
-        infiniteReducer,
-        {loading: true, error: false, data: null}
+        scrollReducer,
+        initialState
     )
 
-    React.useEffect(() => {
+    React.useEffect(() => { 
         dispatch({ type: 'fetch' })
 
-        fetch(url)
-            .then((res) => res.json())
-            .then((data) => dispatch({ type: 'success', data }))
-            .catch((e) => {
-                console.warn(e)
-                dispatch({ type: 'error' })
-            })
-    }, [url])
+        let cancel
 
-    return { 
-        loading: state.loading, 
-        error: state.error, 
-        data: state.data 
-    }
+        Axios({
+            method: 'GET',
+            url: url,
+            params: {page: offset},
+            cancelToken: new Axios.CancelToken(c => cancel = c)
+        }).then(res => {
+            dispatch({ type: 'success', res })
+        }).catch(e => {
+            if(Axios.isCancel(e)) return
+            dispatch({ type: 'error' })
+        })
+
+        return () => cancel()
+    }, [url, offset])
+
+    return {loading: state.loading, error: state.error, data: state.data}
 }
+
+// export default function useInfiniteScroll(url, offset) {
+//     const [loading, setLoading] = React.useState(true)
+//     const [error, setError] = React.useState(false)
+//     const [data, setData] = React.useState([])
+
+//     React.useEffect(() => { 
+//         setLoading(true)
+//         setError(false)
+
+//         let cancel
+
+//         Axios({
+//             method: 'GET',
+//             url: url,
+//             params: {page: offset},
+//             cancelToken: new Axios.CancelToken(c => cancel = c)
+//         }).then(res => {
+//             // setData(res.data)
+//             setData(prevData => {
+//                 return {...prevData, ...res.data}
+//             })
+//             // setData(res.data)
+//             setLoading(false)
+//         }).catch(e => {
+//             if(Axios.isCancel(e)) return
+//             setError(true)
+//         })
+
+//         return () => cancel()
+//     }, [url, offset])
+
+//     return {loading, error, data}
+// }
+
+// function infiniteReducer(state, action) {
+//     if(action.type === 'fetch') {
+//         return {
+//             ...state,
+//             loading: true
+//         }
+//     } else if(action.type === 'success') {
+//         return {
+//             ...state,
+//             loading: false,
+//             data: action.data
+//         }
+//     } else if(action.type === 'error') {
+//         return {
+//             ...state,
+//             loading: false,
+//             error: "Error fetching gifs"
+//         }
+//     } else {
+//         throw new Error('Action not supported, try again.')
+//     }
+// }
+
+// export default function useInfiniteScroll(url) {
+//     const [state, dispatch] = React.useReducer(
+//         infiniteReducer,
+//         {loading: true, error: false, data: null}
+//     )
+
+//     React.useEffect(() => {
+//         dispatch({ type: 'fetch' })
+
+//         fetch(url)
+//             .then((res) => res.json())
+//             .then((data) => dispatch({ type: 'success', data }))
+//             .catch((e) => {
+//                 console.warn(e)
+//                 dispatch({ type: 'error' })
+//             })
+//     }, [url])
+
+//     return { 
+//         loading: state.loading, 
+//         error: state.error, 
+//         data: state.data 
+//     }
+// }
 
 // export default function useInfiniteScroll(url) {
 //     const [loading, setLoading] = React.useState(true)
@@ -60,10 +156,7 @@ export default function useInfiniteScroll(url) {
 //         fetch(url)
 //             .then((res) => res.json())
 //             .then((data) => {
-//                 setData((prevData) => (
-//                     [...prevData, data]
-//                 ))
-//                 // setHasMore(data.length > 0)
+//                 setData(data)
 //                 setLoading(false)
 //                 console.log(data)
 //             }).catch((e) => {
