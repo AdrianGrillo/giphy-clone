39205388ba8cc1c34af5be800b97952d42cc3e6d
Adrian Grillo <64506487+AdrianGrillo@users.noreply.github.com>
2020-10-10 15:06:05 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -23,16 +23,20 @@ function App() {
 
             <Route exact path='/' component={HomeComponents} />
 
-            {/* Regex for our route to capture any number of any combination of characters that the user could search for.
+            {/* Regex for our search route captures any number of any combination of characters that the user could search for.
             Then pass the component being rendered the location prop from react router so we can grab what the user searched for
-            off of the pathname property */}
+            off of the pathname property to display on the page */}
             <Route path='/search=(.*)/' render={({ location }) => <SearchResults location={location} />} />
 
-            <Route path={'/Reactions'} render={({ location }) => <NavLinkedPage location={location} />} />
+            <Route path={'/Reactions'} component={NavLinkedPage} />
 
-            <Route path={'/Entertainment'} render={({ location }) => <NavLinkedPage location={location} />} />
+            <Route path={'/Entertainment'} component={NavLinkedPage} />
 
-            <Route path={'/Sports'} render={({ location }) => <NavLinkedPage location={location} />} />
+            <Route path={'/Sports'} component={NavLinkedPage} />
+
+            <Route path={'/Stickers'} component={NavLinkedPage} />
+
+            <Route path={'/About'} component={NavLinkedPage} />
 
             <Route render={() => <h1>404</h1>} />
 
