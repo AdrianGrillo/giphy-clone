c7b0a14fd1b2b8e1732d8bf2c85e774a070730b4
Adrian Grillo <64506487+AdrianGrillo@users.noreply.github.com>
2020-10-16 13:59:29 -0400

    Add files via upload

---
+++ src/components/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../css/custom-loader.css'
+
+export default function Loading() {
+    return (
+        <div className='spinner'>
+
+        </div>
+    )
+}
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,27 +1,27 @@
 import React from 'react'
 import SearchBar from './search-components/SearchBar'
 import { Link } from 'react-router-dom'
-import { FaEllipsisV, FaUserAlt } from 'react-icons/fa'
-
-const styles = {
-    logo: {
-        width: '180px',
-        height: '60px'
-    },
-    loginText: {
-        position: 'relative',
-        right: '8px'
-    }
-}
+import { FaUserAlt } from 'react-icons/fa'
 
 export default function Nav() {
-    const navLinks = ['Reactions', 'Entertainment', 'Sports', 'Stickers', 'Artists']
+    const navLinks = ['Reactions', 'Entertainment', 'Sports', 'Stickers', 'About']
+
+    const styles = {
+        logo: {
+            width: '180px',
+            height: '60px'
+        },
+        loginText: {
+            position: 'relative',
+            right: '8px'
+        }
+    }
 
     return (
-        <div className='container'>
-            <nav className='flex'>
+        <div>
+            <nav className='flex nav-container'>
                 
-                <Link to='/'><img style={styles.logo} src='https://ucc1c6e29e5c0dc333f0bc673549.previews.dropboxusercontent.com/p/thumb/AA-aQysQrMy_rSNG3l9VdavdeCttCWxrhgRdIFeqy-dOiiTwo5Q2G7Whmv216ZtsFYkTeB1Iw38Pt-w6FSHG8_4t-sDq8wWo0oXXNymJJAE8VZSMiyUsJ1QuZNFGg3HOIbqKlLXuRv0ajRg-ilj6aTHSekDOsFUZ_R14o-p0uA_POVKuQVN9mYrVDPHFgK2431kF7khpgWMiJakQal2BBV-yaHtsrabZZWitI_r9Ls_x3ys17Ty2MxdbXvT56HUQEGikETktC7xIyZ8US4pJQrNhKFIh2n6RuaWLKYcNJHVEPYqPFgESgD2wrksP7Cb3Amn9baxyRV3arUqyjyQlAxaz_5LC1tusLBt7RczxDGO_UA/p.png?fv_content=true&size_mode=5' alt='GIPHY logo' /></Link>
+                <Link to='/'><img style={styles.logo} src='https://i.ibb.co/z5csKC6/giphy-logo.png' alt='GIPHY logo' /></Link>
                 
                 <ul className='row'>
 
@@ -38,13 +38,6 @@ export default function Nav() {
                             </li>
                         )
                     })}
-
-                    <li>
-                        <div className='nav-link nav-link-hover pointer'>
-                            <FaEllipsisV color='white' />
-                        </div>
-                        <div className='nav-color-bar pointer'></div>
-                    </li>
                 </ul>
 
                 <ul className='row'>
@@ -61,7 +54,7 @@ export default function Nav() {
                 </ul>
                 
                 <ul className='row'>
-                <Link className='nav-link login-nav-link' to='/Login'>
+                <Link className='relative nav-link login-nav-link' to='/Login'>
                     <li className='flex relative user-icon-container'>
                         <FaUserAlt color='white' />
                     </li>
