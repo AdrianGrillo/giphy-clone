4b2dd524ee1dc512fb0def589db95cba8cdd679d
Adrian Grillo <64506487+AdrianGrillo@users.noreply.github.com>
2020-10-16 16:37:47 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './css/HomePage.css'
+import './scss/home-page.scss'
 import {
   BrowserRouter as Router,
   Switch, 
@@ -8,42 +8,43 @@ import {
 } from 'react-router-dom'
 import Nav from './components/Nav'
 import HomeComponents from './components/home-components/HomeComponents'
-import SearchResults from './components/search-components/SearchResults'
-import NavLinkedPage from './components/nav-linked-components/NavLinkedPages'
+import Loading from './components/Loading'
+
+// Load these components dynamically only after the user requests them to reduce initial bundle size
+const SearchResults = React.lazy(() => import('./components/search-components/SearchResults'))
+const NavLinkedPage = React.lazy(() => import('./components/nav-linked-components/NavLinkedPages'))
 
 function App() {
   return (
     <Router>
-      <div className='container'>
 
-        <Nav />
-          
-        <React.Suspense>
-          <Switch>
+      <Nav />
+        
+      <React.Suspense fallback={<Loading />}>
+        <Switch>
 
-            <Route exact path='/' component={HomeComponents} />
+          <Route exact path='/' component={HomeComponents} />
 
-            {/* Regex for our search route captures any number of any combination of characters that the user could search for.
-            Then pass the component being rendered the location prop from react router so we can grab what the user searched for
-            off of the pathname property to display on the page */}
-            <Route path='/search=(.*)/' render={({ location }) => <SearchResults location={location} />} />
+          {/* Regex for our search route captures any number of any combination of characters that the user could search for.
+          Then pass the component being rendered the location prop from react router so we can grab what the user searched for
+          off of the pathname property to display on the page */}
+          <Route path='/search=(.*)/' render={({ location }) => <SearchResults location={location} />} />
 
-            <Route path={'/Reactions'} component={NavLinkedPage} />
+          <Route path={'/Reactions'} component={NavLinkedPage} />
 
-            <Route path={'/Entertainment'} component={NavLinkedPage} />
+          <Route path={'/Entertainment'} component={NavLinkedPage} />
 
-            <Route path={'/Sports'} component={NavLinkedPage} />
+          <Route path={'/Sports'} component={NavLinkedPage} />
 
-            <Route path={'/Stickers'} component={NavLinkedPage} />
+          <Route path={'/Stickers'} component={NavLinkedPage} />
 
-            <Route path={'/About'} component={NavLinkedPage} />
+          <Route path={'/About'} component={NavLinkedPage} />
 
-            <Route render={() => <h1>404</h1>} />
+          <Route render={() => <h1>404</h1>} />
 
-          </Switch>
-        </React.Suspense>
+        </Switch>
+      </React.Suspense>
 
-      </div>
     </Router>
   )
 }
